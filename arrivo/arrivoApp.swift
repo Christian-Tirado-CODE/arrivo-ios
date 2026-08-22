801409fed9a53acdf359f3bd36b00e642a43362d
Christian-Tirado-CODE <tiradoc02@gmail.com>
2026-08-22 17:19:35 -0400

    Initial Commit

---
+++ arrivo/arrivoApp.swift
@@ -0,0 +1,17 @@
+//
+//  arrivoApp.swift
+//  arrivo
+//
+//  Created by Christian Tirado on 8/22/26.
+//
+
+import SwiftUI
+
+@main
+struct arrivoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
